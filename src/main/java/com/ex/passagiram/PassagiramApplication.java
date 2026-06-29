7f1ca1397e1fdad30bf185dbad8f6f0d9e51531b
fl0id <f200dm@gmail.com>
2026-06-29 15:49:34 +0300

    Initialize Spring Boot project

---
+++ src/main/java/com/ex/passagiram/PassagiramApplication.java
@@ -0,0 +1,13 @@
+package com.ex.passagiram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassagiramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassagiramApplication.class, args);
+    }
+
+}
